4a83b6f5cca61cbb202d6f04c4e96074b2dc70be
E.W.H.Gunarathna <heshang416@gmail.com>
2026-04-17 12:22:41 +0530

    added exception mappers - handles 409, 422, 403, and 500 errors

---
+++ src/main/java/com/smartcampus/exception/GlobalExceptionMapper.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.exception;
+
+/**
+ *
+ * @author ASUS TUF
+ */
+import com.smartcampus.model.ErrorResponse;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.logging.Logger;
+
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
+
+    private static final Logger LOGGER = Logger.getLogger(GlobalExceptionMapper.class.getName());
+
+    @Override
+    public Response toResponse(Throwable exception) {
+        LOGGER.severe("Unexpected error: " + exception.getMessage());
+        ErrorResponse error = new ErrorResponse(500, "INTERNAL_SERVER_ERROR",
+                "An unexpected error occurred. Please contact support.");
+        return Response.status(500)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
