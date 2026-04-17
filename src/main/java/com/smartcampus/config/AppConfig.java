35ed92bb3f7a7427ca4b7e169a41a87ec61d5a40
E.W.H.Gunarathna <heshang416@gmail.com>
2026-04-17 13:40:43 +0530

    added AppConfig to register all JAX-RS components under /api/v1

---
+++ src/main/java/com/smartcampus/config/AppConfig.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.config;
+
+/**
+ *
+ * @author ASUS TUF
+ */
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("/api/v1")
+public class AppConfig extends ResourceConfig {
+
+    public AppConfig() {
+        packages(
+            "com.smartcampus.resource",
+            "com.smartcampus.exception",
+            "com.smartcampus.filter"
+        );
+    }
+}
